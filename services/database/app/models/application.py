ecd28d662f6452d024b42ea90f38f2ddef7bdfa9
Daren <darenakpinar@gmail.com>
2026-07-21 01:33:20 +0300

    application.py entity attributes with relation to institution.py

--- services/database/app/models/application.py
+++ services/database/app/models/application.py
@@ -1,1 +1,29 @@
 """SQLAlchemy models for mobility applications and their status history."""
+from ..database import db
+
+class MobilityApplication(db.Model):
+    __tablename__ = "mobility_application"
+
+    application_id = db.Column(
+        db.BigInteger,
+        primary_key=True,
+    )
+
+    academic_year = db.Column(
+        db.Date,
+        nullable=False,
+    )
+
+    host_institution_id = db.Column(
+        db.BigInteger,
+        db.ForeignKey(
+            "Institution.institution_id",
+            ondelete="RESTRICT",
+        ),
+        nullable=False,
+    )
+
+    host_institution = db.relationship(
+        "Institution",
+        back_populates="applications",
+    )
